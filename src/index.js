8a957b520212f8b823f8c409be8d32303eaab1bd
RimaSem <rima.semetaite@gmail.com>
2022-12-26 17:17:54 +0200

    Create homepage

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import "./styles.css";
-
 import createHeader from "./header";
+import createHomePage from "./home";
+import createFooter from "./footer";
 
 const container = document.getElementById("content");
-// container.appendChild(createHeader());
-// container.appendChild(createHomePage());
-// // container.appendChild(createFooter());
 
 container.appendChild(createHeader());
-// img.src = Icon;
-// container.appendChild(createFooter());
+container.appendChild(createHomePage());
+container.appendChild(createFooter());
